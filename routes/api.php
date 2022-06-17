fdd927914f7b103d82d90f612a0fb4ed03379411
Derlis Ruiz Diaz <derlisruizdiaz@hotmail.com>
2022-06-17 08:46:09 -0300

    dados

--- routes/api.php
+++ routes/api.php
@@ -14,9 +14,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::get("/hello",function(){
-    return "HOLA";
+
+
+Route::get("/{table}",function($table){
+    return "HOLA".$table;
 });
+
