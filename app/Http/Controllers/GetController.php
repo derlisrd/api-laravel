432833aad656d7004fb2760c0062ac27ce676b0f
derlisrd <derlisruizdiaz@hotmail.com>
2022-06-27 21:28:10 -0400

    derlis

--- app/Http/Controllers/GetController.php
+++ app/Http/Controllers/GetController.php
@@ -13,24 +13,43 @@ class GetController extends Controller
     if (Schema::hasTable($table)) {
 
         $g = $_GET;
-        $fields= (isset($g["fields"]) && !empty($g["fields"]) ) ? explode(",",$g["fields"]) : null;
+        $fields= (isset($g["fields"]) && !empty($g["fields"]) ) ? explode(",",$g["fields"]) : "*";
 
         $tablesingular = substr($table,0,-1);
         $orderBy="id_".$tablesingular;
+
+
 
         $include = isset($g['include']) && !empty($g['include'])  ? explode(",",$g['include'])  : null;
         $on = isset($g['on']) && !empty($g['on'])  ? explode(",",$g['on'])  : null;
         $where = isset($g['where']) && !empty($g['where'])  ? explode(",",$g['where']) : null;
 
 
+        $page = isset($_GET['page']) && !empty($_GET['page'])  ? $_GET['page']  : null;
+        $offset = 0; $limiter = 60;
+        if(!empty($page['size']) && !empty($page['number'])){
+            $offset = intval($page['number']);
+        }
+        else if(!empty($page['size'])){
+            $limiter = intval($page['size']);
+        }
+        else{
+            $limiter =  60;
+        }
+
+
+        $found = DB::table($table)->where($where ? [$where] : null)->count();
+        $total = DB::table($table)->count();
         $res = DB::table($table)
         ->select($fields)
         ->where($where ? [$where] : null)
         ->orderBy($orderBy,"asc")
+        ->offset($offset)
+        ->limit($limiter)
         ->get();
 
 
-        return JsonResponseController::ok($res);
+        return JsonResponseController::OkGet($res,$offset,$limiter,$found,$total);
     }
 
     return JsonResponseController::error("Table not exits");
--- app/Http/Controllers/JsonResponseController.php
+++ app/Http/Controllers/JsonResponseController.php
@@ -3,11 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\URL;
 
 class JsonResponseController extends Controller
 {
-    public static function ok($results){
+    public static function OkGet($results,$pagenumber,$limit,$found,$total){
+
+        $get = request()->all();
+
+        $urlnext = URL::current();
         $array = array(
+            "next"=>$urlnext,
+            "pagenumber"=>$pagenumber,
+            "limit"=>$limit,
+            "found"=>$found,
+            "total"=>$total,
             "response"=>"ok",
             "results"=>$results,
             "error"=>false,
